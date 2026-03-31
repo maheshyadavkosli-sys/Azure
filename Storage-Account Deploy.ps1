913c8d56ee87ab266d79b4e97508a2747fac353d
Mahesh <maheshyadav.kosli@gmail.com>
2026-03-31 11:45:41 -0500

    Filtering

--- Storage-Account Deploy.ps1
+++ Storage-Account Deploy.ps1
@@ -1,31 +1,6 @@
 #Storage-General-Purpose-Account-V2
-
-Get-AzVM | format-table Name, ResourceGroupName, Location 
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the location"
-
-New-AzResourceGroupDeployment -Location $location -ResourceGroupName $resourceGroupName -TemplateUri "C:\Users\E1556093\OneDrive - Emerson\Desktop\Azure\Azure\STA-template.json" -TemplateParameterUri "https://raw.githubusercontent.com/maheshyadavkosli-sys/Azure/refs/heads/main/STA-parameters.json"
-
-#############################################################################
+New-AzResourceGroupDeployment -TemplateFile ".\STA-template.json" -TemplateParameterFile ".\STA-parameters.json"
 
 #Virtual-Machine-Deployment Template
 
-
 New-AzResourceGroupDeployment -TemplateFile .\VM-template.json -TemplateParameterFile .\VM-parameters.json
-
-$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
-New-AzResourceGroupDeployment -TemplateFile .\VM-template.json -TemplateParameterFile .\VM-parameters.json  -adminpassword $adminPassword
-
-
-
-
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$adminUsername = Read-Host -Prompt "Enter the administrator username"
-$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
-$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
-
-New-AzResourceGroup -Name $resourceGroupName -Location "$location"
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\VM-template.json -TemplateParameterFile .\VM-parameters.json -adminUsername $adminUsername -adminPassword $adminPassword -dnsLabelPrefix $dnsLabelPrefix
-
-(Get-AzVm -ResourceGroupName $resourceGroupName).name
