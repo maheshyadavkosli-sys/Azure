bb1f8c38c80245c3d25919d4cb8fbd8a5ef6c69e
maheshyadavkosli-sys <maheshyadav.kosli@gmail.com>
2026-03-31 11:04:43 -0500

    commmit

--- All.ps1
+++ All.ps1
@@ -1,13 +1,14 @@
-# Install IIS 
+#region Install IIS 
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
-#######################################################################################################################################################################################################################################
-#Allow Ping
+#endregion
+
+#region  Ping
 New-NetFirewallRule -DisplayName "Allow Ping Public" -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow -Profile Public -Enabled True
-#######################################################################################################################################################################################################################################
-# Set Timezone to Central 
+#endregion
+#region Set Timezone to Central 
 Set-TimeZone -Name "Central Standard Time"
-#######################################################################################################################################################################################################################################
-# Print Hostname & IP Address to IIS htm file
+#endregion
+#region Print Hostname & IP Address to IIS htm file
 $outputFile = "C:\inetpub\wwwroot\iisstart.htm" # Specify your desired file path
 
 "Hostname: $env:COMPUTERNAME" | Out-File -FilePath $outputFile
@@ -17,14 +18,17 @@ Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -ne "127.0.0.
 
 "--- Done ---" | Out-File -Append -FilePath $outputFile
 Read-Host -Prompt "Press Enter to continue..."
-#######################################################################################################################################################################################################################################
-#Storage-General-Purpose-Account-V2
+#endregion
+
+#region Storage-General-Purpose-Account-V2
+
 Get-AzStorageAccount
 Get-AzVirtualNetwork
 New-AzResourceGroupDeployment -TemplateUri "C:\Users\mahesh\Desktop\STA-Template\STA-template.json" -TemplateParameterFile "C:\Users\mahesh\Desktop\STA-Template\STA-parameters.json"
-################################################################################################################################################################################################################################################
-#Virtual-Machine-Deployment Template
+#endregion
+#region Virtual-Machine-Deployment Template
+
 Get-AzVirtualNetwork
 Get-AzVM
 
-
+#endregion
